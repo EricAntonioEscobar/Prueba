e3f7740ecf65e87912d74b994d0adc980fa535a0
EricEscobar <eescobar@gruponach.com>
2022-04-13 17:17:06 -0500

    first commit

---
+++ backups/respaldo-2022-02-15.sql
@@ -0,0 +1,34 @@
+
+
+
+
+INSERT INTO asesoria VALUES("173107369","Cristina Alexander Vega","2022-01-03","Residencia profesional","Opcion de titulación","Prueba","08:00:00","08:30:00","F","173107154");
+INSERT INTO asesoria VALUES("173107154","Alejandro Terres Gonzalez","2022-01-03","Academica","Asignatura","Prueba","07:00:00","07:30:00","M","173107154");
+INSERT INTO asesoria VALUES("173107176","Viridiana Flores Pineda","2022-01-03","Proyecto","Opcion de titulación","Prueba","12:00:00","12:30:00","F","173107154");
+INSERT INTO asesoria VALUES("163107857","Jaun Sepulveda","2022-01-03","Residencia profesional","Opcion de titulación","Prueba","14:00:00","14:30:00","M","173107122");
+INSERT INTO asesoria VALUES("173107321","Maribel Peréz Peréz","2022-01-05","Residencia profesional","Opcion de titulación","Prueba","06:00:00","06:30:00","F","173107122");
+INSERT INTO asesoria VALUES("163107367","Crsitina Aguilera","2022-01-06","Residencia profesional","Opcion de titulación","Prueba","07:00:00","07:30:00","F","173107122");
+INSERT INTO asesoria VALUES("173107749","Cristobal Colon","2022-01-07","Proyecto","Opcion de titulación","Prueba","13:00:00","13:30:00","M","173107122");
+INSERT INTO asesoria VALUES("163107521","Melanie Santiago Gimenez","2022-01-07","Academica","Asignatura","Prueba","14:00:00","14:30:00","F","173107122");
+INSERT INTO asesoria VALUES("163107679","Federico Guzman Loera","2022-02-01","Academica","Asignatura","prueba","08:00:00","08:30:00","M","173107154");
+INSERT INTO asesoria VALUES("173107456","Valeria Perez Gonzalez","2022-02-07","Residencia profesional","Opcion de titulación","Prueba","13:00:00","13:30:00","F","173107154");
+INSERT INTO asesoria VALUES("163107850","Archivaldo Lopez","2022-02-14","Titulacion","Proyecto","Prueba","09:00:00","09:30:00","M","173107122");
+INSERT INTO asesoria VALUES("173107159","Fernanda Lopez Cornejo","2022-02-15","Residencia profesional","Opcion de titulación","Prueba","12:00:00","12:30:00","F","173107122");
+INSERT INTO asesoria VALUES("173107127","Crsitiano Ronaldo Oliviera","2022-02-21","Titulacion","Proyecto","Prueba","03:00:00","13:30:00","M","173107122");
+INSERT INTO asesoria VALUES("16310346","Carolina Hernandez Peréz","2022-02-25","Academica","Asignatura","Prueba","11:00:00","11:30:00","F","173107154");
+INSERT INTO asesoria VALUES("173107834","Franco Escamilla","2022-03-01","Proyecto","Opcion de titulación","Prueba","14:00:00","14:30:00","M","173107154");
+INSERT INTO asesoria VALUES("163107546","Virginia Ramirez","2022-03-02","Residencia profesional","Opcion de titulación","Prueba","10:00:00","10:30:00","F","173107154");
+INSERT INTO asesoria VALUES("173107879","Rodolfo Pizarro","2022-03-03","Academica","Asignatura","Prueba","06:00:00","06:30:00","M","173107154");
+INSERT INTO asesoria VALUES("173107561","Aldhair Gonzalez Laguna","2022-03-14","Residencia profesional","Opcion de titulación","Prueba","10:00:00","10:30:00","M","173107154");
+INSERT INTO asesoria VALUES("163107857","Melissa Daniel Medina","2022-03-08","Residencia profesional","Opcion de titulación","Prueba","09:00:00","09:30:00","F","173107154");
+INSERT INTO asesoria VALUES("17310767","Georgina Ortega","2022-03-21","Titulacion","Proyecto","Prueba","08:00:00","08:30:00","F","173107154");
+INSERT INTO asesoria VALUES("163107888","Mario Castañeda","2022-03-28","Proyecto","Opcion de titulación","Prueba","13:00:00","13:30:00","M","173107122");
+INSERT INTO asesoria VALUES("163107999","Cassadra Olivares Sainz","2022-03-29","Proyecto","Opcion de titulación","Prueba","12:00:00","12:30:00","F","173107122");
+INSERT INTO asesoria VALUES("173107555","Israel Castro Paz","2022-03-31","Proyecto","Opcion de titulación","Prueba","11:00:00","11:30:00","M","173107122");
+INSERT INTO asesoria VALUES("173107666","Gabriela Berenice","2022-03-21","Academica","Asignatura","Prueba","12:00:00","12:30:00","F","173107122");
+INSERT INTO asesoria VALUES("173107444","Bernardo Silva","2022-03-22","Titulacion","Proyecto","Prueba","10:00:00","10:30:00","M","173107122");
+INSERT INTO asesoria VALUES("173107111","Carmelita Slinas","2022-03-24","Residencia profesional","Opcion de titulación","Prueba","14:00:00","14:30:00","F","173107122");
+INSERT INTO asesoria VALUES("173107245","Rut Juarez","2022-02-14","Residencia profesional","Opcion de titulación","Prueba","09:00:00","09:30:00","F","173107122");
+
+
+
